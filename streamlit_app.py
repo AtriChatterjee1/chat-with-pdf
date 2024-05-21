fec5a440f8294037eb1c868d1bd0b53cef39143c
AtriChatterjee1 <print.atri@gmail.com>
2024-05-21 21:27:41 +0530

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import openai
-from llama_index.llms.llama-api import LlamaAPI
+from llama_index.llms.llama_api import LlamaAPI
 from llama_index.llms.openai import OpenAI
 try:
   from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
